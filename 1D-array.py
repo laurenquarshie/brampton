976fc205477379e2288d40c6a167927cd7d82a31
37070 <lauren.quarshie@gmail.com>
2017-11-07 10:21:52 +0000

    display array

--- 1D-array.py
+++ 1D-array.py
@@ -2,9 +2,20 @@
 # 7/11/17
 
 # DECLARE Ages : ARRAY(0..9) OF INTEGER
-Ages = [3,4,5,88,45,12,36,48,40,89]
-#for index in range(9, -1, -1):
-    #print (Ages[index])
+Ages = []
+for index in range (0,10):
+ num = int(input("enter a number between zero and 10"))
+ while num > 10 or num < 0:
+  print("error - not within range")
+  input("enter a number between 0 and 10")
+
+
+ Ages.append(num)
+
+
+#Ages = [3,4,5,88,45,12,36,48,40,89]
+
+
 
 for index in range(0,10):
         for x in range (0,9):
