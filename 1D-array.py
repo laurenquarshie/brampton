ee296cf3e4b94c477d039e560e27d69065bd417f
37070 <lauren.quarshie@gmail.com>
2017-11-07 10:03:53 +0000

    display array

--- 1D-array.py
+++ 1D-array.py
@@ -3,5 +3,14 @@
 
 # DECLARE Ages : ARRAY(0..9) OF INTEGER
 Ages = [3,4,5,88,45,12,36,48,40,89]
+#for index in range(9, -1, -1):
+    #print (Ages[index])
+
 for index in range(0,10):
-    print (Ages[index])
+        for x in range (0,9):
+         if Ages[x] > Ages[x + 1] :
+          Temp = Ages[x]
+          Ages[x] = Ages[x + 1]
+          Ages[x + 1] = Temp
+for index in range (0,10):
+ print (Ages[index])
